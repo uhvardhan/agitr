1c94ef60a7e75e47bfe4d4f2c0dc866426fa7d27
uhvardhan <uhv93@terpmail.umd.edu>
2019-09-26 17:29:07 -0400

    Chapter 5 completed

--- count.cpp
+++ count.cpp
@@ -3,12 +3,12 @@
 #include <ros/ros.h>
  int main(int argc, char**argv) {
      // Initialize the ROS system and become a node
-     ros::init(argc, argc, "count_and_log");
+     ros::init(argc, argv, "count_and_log");
      ros::NodeHandle nh;
 
      // Generate log messages of varying severity regularly.
      ros::Rate rate(10);
-     for(init i = 1;ros::ok(); i++) {
+     for(int i = 1;ros::ok(); i++) {
          ROS_DEBUG_STREAM("Counted to " << i);
          if ((i%3) == 0) {
              ROS_INFO_STREAM(i << " is divisible by 3.");
